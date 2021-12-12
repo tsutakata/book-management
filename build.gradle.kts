7854e32b9d77954efb78beaff859c55b6c240079
tsutakata <akt216@gmail.com>
2021-12-12 18:40:43 +0900

    build.gradle修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,24 +18,16 @@ repositories {
 
 dependencies {
 	runtimeOnly("com.h2database:h2")
-	runtimeOnly("org.postgresql:postgresql")
 	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.kotest:kotest-runner-junit5-jvm:5.0.0")
-	testImplementation("io.kotest:kotest-runner-junit5:5.0.1")
 	testImplementation("io.kotest:kotest-assertions-core:5.0.1")
-	testImplementation("io.kotest:kotest-runner-console-jvm:4.1.3")
-	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.0")
 	testImplementation("io.rest-assured:rest-assured:4.4.0")
-	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
 }
 
 tasks.withType<KotlinCompile> {
